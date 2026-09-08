ff7f124901f6df0823efa209225d83d55c57a39c
Lou Evora <juseve0531@gmail.com>
2026-09-08 12:56:04 +0800

    Revert "Allow local user data without enabling Render database"
    This reverts commit 31d82b5de3833ccb7bbae8083bbf944b13fabc2c.

--- app/controllers/UserController.php
+++ app/controllers/UserController.php
@@ -10,18 +10,10 @@ class UserController extends Controller {
     public function __construct()
     {
         parent::__construct();
-
-        if (getenv('DB_ENABLED') === 'true') {
-            $this->call->model('UserModel');
-        }
     }
 
     public function index()
     {
-        $users = getenv('DB_ENABLED') === 'true'
-            ? $this->UserModel->all()
-            : [];
-
-        $this->call->view('users', ['users' => $users]);
+        $this->call->view('users', ['users' => []]);
     }
 }
