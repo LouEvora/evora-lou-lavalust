a6aa106d36025f15cb00b93377a1ec6da91b41cb
Lou Evora <juseve0531@gmail.com>
2026-09-08 12:56:04 +0800

    Revert "Remove database dependency from web routes"
    This reverts commit 5fee63713d7d5faff68d200342f5611afb1e625c.

--- app/controllers/UserController.php
+++ app/controllers/UserController.php
@@ -10,10 +10,12 @@ class UserController extends Controller {
     public function __construct()
     {
         parent::__construct();
+        $this->call->model('UserModel');
     }
 
     public function index()
     {
-        $this->call->view('users', ['users' => []]);
+        $users = $this->UserModel->all();
+        $this->call->view('users', ['users' => $users]);
     }
 }
