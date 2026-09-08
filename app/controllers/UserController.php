96631b476f8062fdfa619feb7a0324561a53b399
Lou Evora <juseve0531@gmail.com>
2026-09-08 11:43:23 +0800

    Show users without a database connection

--- app/controllers/UserController.php
+++ app/controllers/UserController.php
@@ -20,7 +20,50 @@ class UserController extends Controller {
     {
         $users = getenv('DB_ENABLED') === 'true'
             ? $this->UserModel->all()
-            : [];
+            : [
+                [
+                    'id' => 1,
+                    'firstname' => 'Juan',
+                    'lastname' => 'Dela Cruz',
+                    'email' => 'juan@gmail.com',
+                    'username' => 'juandelacruz',
+                ],
+                [
+                    'id' => 2,
+                    'firstname' => 'Maria',
+                    'lastname' => 'Santos',
+                    'email' => 'maria@gmail.com',
+                    'username' => 'mariasantos',
+                ],
+                [
+                    'id' => 3,
+                    'firstname' => 'Pedro',
+                    'lastname' => 'Garcia',
+                    'email' => 'pedro@gmail.com',
+                    'username' => 'pedrogarcia',
+                ],
+                [
+                    'id' => 4,
+                    'firstname' => 'John',
+                    'lastname' => 'Doe',
+                    'email' => 'djohn@gmail.com',
+                    'username' => 'johndoe',
+                ],
+                [
+                    'id' => 5,
+                    'firstname' => 'Mike',
+                    'lastname' => 'Wazowski',
+                    'email' => 'mikewaza@gmail.com',
+                    'username' => 'mikewazowski',
+                ],
+                [
+                    'id' => 6,
+                    'firstname' => 'Mary',
+                    'lastname' => 'Jane',
+                    'email' => 'maryjane@gmail.com',
+                    'username' => 'maryjane',
+                ],
+            ];
 
         $this->call->view('users', ['users' => $users]);
     }
