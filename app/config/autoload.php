b87044fd7dda945442fc63d47e7bf69614202a15
Lou Evora <juseve0531@gmail.com>
2026-09-08 12:56:04 +0800

    Revert "Enable local database access explicitly"
    This reverts commit 37849a76437b3dc5bc472f90084d8eef5f19da6e.

--- app/config/autoload.php
+++ app/config/autoload.php
@@ -72,9 +72,7 @@ defined('PREVENT_DIRECT_ACCESS') OR exit('No direct script access allowed');
 |
 |	$autoload['libraries'] = array('database', 'email', 'session');
 */
-$autoload['libraries'] = getenv('DB_ENABLED') === 'true'
-	? array('database')
-	: array();
+$autoload['libraries'] = array();
 
 /*
 | -------------------------------------------------------------------
